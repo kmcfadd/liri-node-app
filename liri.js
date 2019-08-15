6238c0e67810d5858f8cb77b5490ed8e9d207134
kmcfadd <39887165+kmcfadd@users.noreply.github.com>
2019-08-14 18:48:34 -0700

    add functionality to do-what-it-says input command

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+// all node packages required and linked, including file system to read and write files
 require("dotenv").config();
 
 var fs = require('fs');
@@ -5,16 +6,15 @@ const axios = require('axios');
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var moment = require('moment');
-
-
+// safeguard spotify developer keys
 var spotify = new Spotify(keys.spotify);
-
+// take in user input here to be passed into the switch cases
 var input = process.argv[2]
-
+// general variable declaration for each case
 var movie;
 var song;
 var artist;
-
+// a switch case to separate each liri mode by user input
 switch (input) {
     case 'concert-this':
         getConcert();
@@ -27,26 +27,21 @@ switch (input) {
         break
     case 'do-what-it-says':
         doThing();
-        break
 }
 
 // concert-this
-
 function getConcert() {
 
     artist = process.argv.slice(3).join("+")
 
-    axios.get('https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp').then(function (response) {
-
+    axios.get('https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp')
+        .then(function (response) {
 
             for (var i = 0; i < response.data.length; i++) {
-
                 console.log(response.data[i].venue.name + " " +
                     response.data[i].venue.country + ", " + response.data[i].venue.city + ", " + response.data[i].venue.region + " " +
                     moment(response.data[i].venue.datetime).format("MM/DD/YYYY"))
-
             }
-
         })
         .catch(function (err) {
             console.log(err)
@@ -54,19 +49,19 @@ function getConcert() {
 }
 
 // spotify-this
-
 function getSong() {
 
-    song = process.argv.slice(3).join("+")
-
+    if (!process.argv[3]) {
+        song = 'Ace of Base'
+    } else {
+        song = process.argv.slice(3).join("+")
+    }
     spotify.search({
             type: 'track',
             query: song,
             limit: 1
         })
         .then(function (data) {
-
-            //console.log(JSON.stringify(data, null, 2));
             console.log("Artist: " + data.tracks.items[0].album.artists[0].name) // artist name
             console.log("Song: " + data.tracks.items[0].name) // song name
             console.log("Album: " + data.tracks.items[0].album.name) // album name
@@ -76,10 +71,9 @@ function getSong() {
 
             console.log(err)
         })
-
 }
-// movie-this
 
+// movie-this
 function getMovie() {
 
     if (!process.argv[3]) {
@@ -100,21 +94,67 @@ function getMovie() {
             console.log("Actors: " + response.data.Actors) // actors in movie
         })
         .catch(function (err) {
-            if (err.response) {
-                console.log("------Data-----")
-                console.log(err.response.data)
-                console.log("-----Status-----")
-                console.log(err.response.status)
-                console.log("-----Status-----")
-                console.log(err.response.headers)
-
-            } else if (err.request) {
-                console.log(err.request)
-            } else {
-                console.log("Error", err.message)
-            }
-            console.log(err.config);
+            console.log(err)
         })
-
 }
+
 // do-what-it-says
+function doThing() {
+
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err)
+        }
+        var info = data.split(',')
+
+        var command = info[0]
+        var item = info[1]
+
+        switch (command) {
+            case 'concert-this':
+                axios.get('https://rest.bandsintown.com/artists/' + item + '/events?app_id=codingbootcamp')
+                    .then(function (response) {
+                        for (var i = 0; i < response.data.length; i++) {
+                            console.log(response.data[i].venue.name + " " +
+                                response.data[i].venue.country + ", " + response.data[i].venue.city + ", " + response.data[i].venue.region + " " +
+                                moment(response.data[i].venue.datetime).format("MM/DD/YYYY"))
+                        }
+                    })
+                    .catch(function (err) {
+                        console.log(err)
+                    })
+                break
+            case 'spotify-this':
+                spotify.search({
+                        type: 'track',
+                        query: item,
+                        limit: 1
+                    })
+                    .then(function (data) {
+                        console.log("Artist: " + data.tracks.items[0].album.artists[0].name) // artist name
+                        console.log("Song: " + data.tracks.items[0].name) // song name
+                        console.log("Album: " + data.tracks.items[0].album.name) // album name
+                        console.log("Preview: " + data.tracks.items[0].preview_url) // preview url 
+                    })
+                    .catch(function (err) {
+                        console.log(err)
+                    })
+                break
+            case 'movie-this':
+                axios.get('https://www.omdbapi.com/?t=' + item + '&y=&plot=short&apikey=trilogy')
+                    .then(function (response) {
+                        console.log(response.data.Title) // title of the movie
+                        console.log("Released in " + response.data.Year) // year movie came out
+                        console.log("IMDB Rating " + response.data.Ratings[i].Value) // imdb rating
+                        console.log("Rotten Tomatoes Rating " + response.data.Ratings[1].Value) // rotten tomatoes rating
+                        console.log("Produced in " + response.data.Country) // country movie was produced
+                        console.log("Available in " + response.data.Language) // language of movie
+                        console.log("Plot: " + response.data.Plot) // plot of movie
+                        console.log("Actors: " + response.data.Actors) // actors in movie
+                    })
+                    .catch(function (err) {
+                        console.log(err)
+                    })
+        }
+    })
+}
